2043cdedd53e50061cf963091ef105dd9f674cd1
rans0000 <rans0000@gmail.com>
2024-02-17 15:28:45 +0530

    basic particles: add opacity to particles

--- src/libs/particle.ts
+++ src/libs/particle.ts
@@ -2,6 +2,7 @@ import P5 from 'p5';
 import Entity from './entity';
 
 class Particle extends Entity {
+    delta: number;
     constructor(
         readonly p5: P5,
         readonly collection: Entity[],
@@ -10,17 +11,23 @@ class Particle extends Entity {
         r: number = 5
     ) {
         super(p5, collection, x, y, r);
+        this.delta = p5.random(255);
     }
 
     process(deltaTime: number): void {
         this.x += this.p5.random(-2, 2);
         this.y += this.p5.random(-2, 2);
+        this.delta = (this.delta + deltaTime / 100) % 255;
     }
 
     draw(deltaTime: number): void {
         this.process(deltaTime);
         this.p5.noStroke();
-        this.p5.fill(this.p5.random(150, 200), this.p5.random(0, 140), this.p5.random(40, 80));
+        const h = this.p5.noise(this.delta) * 255;
+        const s = this.p5.noise(this.delta + 10) * 255;
+        const v = this.p5.noise(this.delta + 100) * 255;
+
+        this.p5.fill(h, s, v, this.p5.random(0.1));
         this.p5.ellipse(this.x, this.y, this.r, this.r);
     }
 }
