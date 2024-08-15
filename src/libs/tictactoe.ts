e8cda0bec5e60395fd0c7a2c76e8f2ef771631da
rans0000 <rans0000@gmail.com>
2024-08-15 17:15:47 +0530

    add 'Tic Tac Toe' sketch

---
+++ src/libs/tictactoe.ts
@@ -0,0 +1,114 @@
+import P5 from 'p5';
+import { Gamer } from 'src/utils/utils';
+
+type TCell = {
+    pos: P5.Vector;
+    owner: Gamer | undefined;
+};
+
+type TTicTacToe = {
+    showHelpers: boolean;
+    cells: TCell[];
+    size: number;
+    currentTurn: Gamer;
+};
+
+type Keys = 'size';
+
+class TicTacToe {
+    p5: P5;
+    cells: TCell[];
+    showHelpers: boolean;
+    size: number;
+    currentTurn: Gamer;
+
+    constructor(p5: P5, _config?: Partial<TTicTacToe>) {
+        const config: TTicTacToe = {
+            showHelpers: false,
+            cells: buildCells(p5),
+            size: 300,
+            currentTurn: Gamer.PLAYER,
+            ..._config
+        };
+        this.p5 = p5;
+        this.showHelpers = config.showHelpers;
+        this.cells = config.cells;
+        this.size = config.size;
+        this.currentTurn = config.currentTurn;
+    }
+
+    setValues(key: Keys, value: number) {
+        switch (key) {
+            case 'size':
+                this.size = value;
+                break;
+
+            default:
+                throw 'Unsupported key passed to setValues()';
+        }
+    }
+
+    update(deltaTime: number): this {
+        return this;
+    }
+
+    draw(): this {
+        this.p5.cursor(this.p5.ARROW);
+        const width = this.size / 3;
+        const { innerWidth, innerHeight } = window;
+        const offsetX = (innerWidth - this.size) / 2;
+        const offsetY = (innerHeight - this.size) / 2;
+        const size = 3;
+        const { mouseX, mouseY } = this.p5;
+
+        for (const cell of this.cells) {
+            const startX = offsetX + cell.pos.x * width;
+            const startY = offsetY + cell.pos.y * width;
+            //draw the cells
+            this.p5.strokeWeight(1);
+            this.p5.stroke(255);
+            this.p5.fill(40, 50, 10);
+            this.p5.rectMode(this.p5.CORNER);
+            this.p5.rect(startX, startY, width, width);
+
+            if (cell.owner !== undefined) {
+                this.p5.strokeWeight(0);
+                this.p5.fill(255);
+                this.p5.textAlign(this.p5.CENTER, this.p5.CENTER);
+                this.p5.textSize(this.size / size);
+                const text = cell.owner === Gamer.PLAYER ? 'X' : 'O';
+                this.p5.text(text, startX + this.size / size / 2, startY + this.size / size / 2);
+            }
+
+            // show valid cells
+            if (
+                cell.owner === undefined &&
+                mouseX > startX &&
+                mouseX < startX + this.size / size &&
+                mouseY > startY &&
+                mouseY < startY + this.size / size
+            ) {
+                this.p5.cursor(this.p5.HAND);
+            }
+        }
+        return this;
+    }
+}
+
+/**--------------------------------- */
+// functions
+
+function buildCells(p5: P5): TCell[] {
+    const size = 3;
+    const cells: TCell[] = [];
+    for (let i = 0; i < size * size; i++) {
+        // let owner: Gamer | undefined = Math.floor(p5.random(2));
+        cells.push({
+            pos: p5.createVector(Math.floor(i / size), i % size),
+            owner: undefined
+        });
+    }
+    return cells;
+}
+
+export default TicTacToe;
---
+++ src/scripts/tictactoe-game.ts
@@ -0,0 +1,63 @@
+import { GUI } from 'dat.gui';
+import P5 from 'p5';
+import TicTacToe from 'src/libs/tictactoe';
+
+/**--------------------------------- */
+// variables & types
+let board: TicTacToe;
+
+/**--------------------------------- */
+// sketch
+const sketch = (p5: P5) => {
+    const options = {
+        showHelpers: true,
+        size: 400
+    };
+
+    const gui = new GUI({ autoPlace: false });
+    gui.domElement.id = 'gui';
+    document.getElementById('gui')?.appendChild(gui.domElement);
+    gui.add(options, 'showHelpers');
+    gui.add(options, 'size', 200, 500, 10).onChange((val) => {
+        board.setValues('size', val);
+    });
+
+    /** setup */
+    p5.setup = () => {
+        const canvas = p5.createCanvas(window.innerWidth, window.innerHeight);
+        canvas.parent('app');
+        p5.background('white');
+        p5.pixelDensity(1);
+        p5.colorMode(p5.HSB);
+        window.addEventListener('resize', () => resizeDisplay(p5));
+        init(p5);
+        // p5.noLoop();
+
+        // setup the board
+        board = new TicTacToe(p5, { showHelpers: options.showHelpers, size: options.size });
+        console.log(board.cells.map((t) => t.owner));
+    };
+
+    /** draw */
+    p5.draw = () => {
+        p5.background(200, 60, 10);
+
+        board.update(p5.deltaTime).draw();
+    };
+
+    /**--------------------------------- */
+    // functions
+
+    function resizeDisplay(canvas: P5) {
+        canvas.resizeCanvas(window.innerWidth, window.innerHeight);
+    }
+
+    function init(p5: P5) {
+        p5.background(200, 60, 10);
+    }
+
+    /**--------------------------------- */
+    // classes
+};
+
+new P5(sketch);
