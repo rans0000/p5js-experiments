5130ae536958ea0b4e50df7e00a41dc011008b15
rans0000 <rans0000@gmail.com>
2024-10-22 00:20:18 +0530

    L-system: create a basic fractal tree

--- src/libs/fractal-tree.ts
+++ src/libs/fractal-tree.ts
@@ -1,22 +1,71 @@
 import P5 from 'p5';
 
 type TFractalTree = {
+    initialVocab: string;
+    vocab: string;
+    grammar: Record<string, string>;
+    branchLength: number;
+    branchAngle: number;
+    leafRadius: number;
     showHelpers: boolean;
 };
+type TFractalTreeConfig = Omit<TFractalTree, 'grammar' | 'vocab'> & { rule: string };
 
 type Keys = 'size' | 'showHelpers';
 
 class FractalTree {
     p5: P5;
+    rule: string;
+    initialVocab: string;
+    vocab: string;
+    grammar: Record<string, string>;
+    branchLength: number;
+    branchAngle: number;
+    leafRadius: number;
     showHelpers: boolean;
 
-    constructor(p5: P5, _config?: Partial<TFractalTree>) {
-        const config: TFractalTree = {
+    constructor(p5: P5, _config?: Partial<TFractalTreeConfig>) {
+        const config: TFractalTreeConfig = {
+            initialVocab: 'S',
+            rule: 'S:FB F:FF B:[lFB][rFB]',
+            branchLength: 20,
+            branchAngle: Math.PI / 4,
+            leafRadius: 3,
             showHelpers: false,
             ..._config
         };
         this.p5 = p5;
+        this.vocab = config.initialVocab;
+        this.initialVocab = config.initialVocab;
+        this.grammar = this.generateGrammar(config.rule);
+        this.branchLength = config.branchLength;
+        this.branchAngle = config.branchAngle;
+        this.leafRadius = config.leafRadius;
         this.showHelpers = config.showHelpers;
+    }
+
+    private generateGrammar(rule: string): Record<string, string> {
+        let obj: Record<string, string> = {};
+        rule.split(' ').map((elem) => {
+            const item = elem.split(':');
+            obj[item[0]] = item[1];
+        });
+        return obj;
+    }
+
+    generateVocab(iterations: number = 6): string {
+        let str = '';
+        let vocab = this.initialVocab;
+
+        for (let i = 0; i < iterations; i++) {
+            str = '';
+
+            for (const letter of vocab) {
+                str += this.grammar.hasOwnProperty(letter) ? this.grammar[letter] : letter;
+            }
+            vocab = str;
+        }
+        return (this.vocab = vocab);
     }
 
     setValues(key: Keys, value: number | boolean) {
@@ -42,6 +91,35 @@ class FractalTree {
     }
 
     draw(): this {
+        for (const letter of this.vocab) {
+            switch (letter) {
+                case 'F':
+                    this.p5.stroke(255);
+                    this.p5.strokeWeight(2);
+                    this.p5.line(0, 0, 0, -this.branchLength);
+                    this.p5.translate(0, -this.branchLength);
+                    break;
+                case 'B':
+                    this.p5.stroke(255);
+                    this.p5.strokeWeight(2);
+                    this.p5.circle(0, 0, this.leafRadius);
+                    break;
+                case 'l':
+                    this.p5.rotate(-this.branchAngle);
+                    break;
+                case 'r':
+                    this.p5.rotate(this.branchAngle);
+                    break;
+                case '[':
+                    this.p5.push();
+                    break;
+                case ']':
+                    this.p5.pop();
+                    break;
+                default:
+                    break;
+            }
+        }
         return this;
     }
 }
--- src/scripts/l-system.ts
+++ src/scripts/l-system.ts
@@ -24,6 +24,7 @@ const sketch = (p5: P5) => {
         p5.background('white');
         p5.pixelDensity(1);
         p5.colorMode(p5.HSB);
+        p5.noLoop();
         window.addEventListener('resize', () => resizeDisplay(p5));
 
         //
@@ -33,7 +34,7 @@ const sketch = (p5: P5) => {
     /** draw */
     p5.draw = () => {
         p5.background(200, 60, 10);
-
+        p5.translate(window.innerWidth / 2, window.innerHeight - 100);
         tree.update(p5.deltaTime).draw();
     };
 
@@ -46,7 +47,10 @@ const sketch = (p5: P5) => {
 
     function init(p5: P5) {
         p5.background(200, 60, 10);
-        tree = new FractalTree(p5);
+        tree = new FractalTree(p5, {
+            branchLength: 3
+        });
+        tree.generateVocab(7);
     }
 
     /**--------------------------------- */
