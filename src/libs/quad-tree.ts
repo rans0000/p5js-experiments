41e607d6df54d266a58258c435ddd33db7f9644f
rans0000 <rans0000@gmail.com>
2024-10-25 00:45:13 +0530

    Quad Tree: add a basic quad tree to insert items

--- src/libs/quad-tree.ts
+++ src/libs/quad-tree.ts
@@ -1,44 +1,121 @@
 import P5 from 'p5';
 
+interface TPoint {
+    x: number;
+    y: number;
+}
+
 type TQuadTreeConfig = {
-    maxItems: number;
+    capacity: number;
     width: number;
     height: number;
+    pos: TPoint;
 };
 
 class QuadTree {
     p5: P5;
-    quads: Quad[];
-    maxItems: number;
+    quad: Quad;
+    subdivisions: QuadTree[];
+    capacity: number;
+    items: TPoint[];
 
-    constructor(p5: P5, _config: TQuadTreeConfig) {
-        const config: TQuadTreeConfig = {
-            ..._config
-        };
-
+    constructor(p5: P5, config: TQuadTreeConfig) {
         this.p5 = p5;
-        this.quads = [];
-        this.maxItems = config.maxItems;
+        this.items = [];
+        this.quad = this.generateQuad(config.pos, config.width, config.height);
+        this.subdivisions = [];
+        this.capacity = config.capacity;
     }
 
-    update(_deltaTime: number): this {
-        return this;
+    generateQuad(pos: TPoint, width: number, height: number) {
+        const quad = new Quad(this.p5, pos, width, height);
+        return quad;
+    }
+
+    subdivide() {
+        const quad = this.quad;
+        const w = quad.width / 4;
+        const h = quad.height / 4;
+
+        const ne = new QuadTree(this.p5, {
+            capacity: this.capacity,
+            height: h * 2,
+            width: w * 2,
+            pos: { x: quad.pos.x + w, y: quad.pos.y + h }
+        });
+        const nw = new QuadTree(this.p5, {
+            capacity: this.capacity,
+            height: h * 2,
+            width: w * 2,
+            pos: { x: quad.pos.x - w, y: quad.pos.y + h }
+        });
+        const se = new QuadTree(this.p5, {
+            capacity: this.capacity,
+            height: h * 2,
+            width: w * 2,
+            pos: { x: quad.pos.x + w, y: quad.pos.y - h }
+        });
+        const sw = new QuadTree(this.p5, {
+            capacity: this.capacity,
+            height: h * 2,
+            width: w * 2,
+            pos: { x: quad.pos.x - w, y: quad.pos.y - h }
+        });
+        this.subdivisions = [ne, nw, se, sw];
+    }
+
+    insert(item: TPoint) {
+        if (!this.quad.isWithinBounds(item)) return;
+
+        if (this.items.length < this.capacity) {
+            this.items.push(item);
+            return;
+        }
+        if (!this.subdivisions.length) {
+            this.subdivide();
+        }
+        this.subdivisions.map((quad) => quad.insert(item));
     }
 
     draw(): this {
+        this.quad.draw();
+        for (const quad of this.subdivisions) {
+            quad.draw();
+        }
+        for (const item of this.items) {
+            this.p5.stroke(255, 1);
+            this.p5.circle(item.x, item.y, 2);
+        }
         return this;
     }
 }
 
 class Quad {
-    pos: P5.Vector;
+    p5: P5;
+    pos: TPoint;
     width: number;
     height: number;
 
-    constructor(pos: P5.Vector, width: number, height: number) {
+    constructor(p5: P5, pos: TPoint, width: number, height: number) {
+        this.p5 = p5;
         this.pos = pos;
         this.width = width;
         this.height = height;
+    }
+
+    isWithinBounds(point: TPoint): boolean {
+        const { x, y } = this.pos;
+        const w = this.width / 2;
+        const h = this.height / 2;
+        return point.y <= y + h && point.x <= x + w && point.y > y - h && point.x > x - w;
+    }
+
+    draw(): this {
+        this.p5.rectMode(this.p5.CENTER);
+        this.p5.stroke(255);
+        this.p5.noFill();
+        this.p5.rect(this.pos.x, this.pos.y, this.width, this.height);
+        return this;
     }
 }
 
--- src/scripts/quad-tree-demo.ts
+++ src/scripts/quad-tree-demo.ts
@@ -1,9 +1,10 @@
 import { GUI } from 'dat.gui';
 import P5 from 'p5';
 import QuadTree from 'src/libs/quad-tree';
+import { MOUSE_BTN } from 'src/utils/utils';
 
 /**--------------------------------- */
-let tree: QuadTree;
+let qtree: QuadTree;
 
 /**--------------------------------- */
 // sketch
@@ -21,7 +22,6 @@ const sketch = (p5: P5) => {
         p5.background('white');
         p5.pixelDensity(1);
         p5.colorMode(p5.HSB);
-        p5.frameRate(1);
         window.addEventListener('resize', () => resizeDisplay(p5));
 
         //
@@ -31,6 +31,13 @@ const sketch = (p5: P5) => {
     /** draw */
     p5.draw = () => {
         p5.background(200, 60, 10);
+        qtree.draw();
+    };
+
+    p5.mouseReleased = (e: MouseEvent) => {
+        if (e.button !== MOUSE_BTN.LEFT) return;
+        qtree.insert({ x: e.clientX, y: e.clientY });
+        console.log(qtree);
     };
 
     /**--------------------------------- */
@@ -41,7 +48,20 @@ const sketch = (p5: P5) => {
     }
 
     function init(p5: P5) {
+        const size = 500;
+        const { innerWidth, innerHeight } = window;
         p5.background(200, 60, 10);
+        const pos = p5.createVector(innerWidth / 2, innerHeight / 2);
+        qtree = new QuadTree(p5, { pos, capacity: 4, width: size, height: size });
+
+        // draw points
+        for (let i = 0; i < 40; i++) {
+            const pos = {
+                x: p5.random(size) + (innerWidth / 2 - size / 2),
+                y: p5.random(size) + (innerHeight / 2 - size / 2)
+            };
+            qtree.insert(pos);
+        }
     }
 
     /**--------------------------------- */
